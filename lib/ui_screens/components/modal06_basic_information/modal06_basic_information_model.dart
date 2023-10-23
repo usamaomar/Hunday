0727b204cd22011494bc76c2da13d92dfb77aef7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-23 08:21:31 +0000

    Updating to latest FlutterFlow output.

--- lib/ui_screens/components/modal06_basic_information/modal06_basic_information_model.dart
+++ lib/ui_screens/components/modal06_basic_information/modal06_basic_information_model.dart
@@ -2,7 +2,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:ui';
 import 'modal06_basic_information_widget.dart'
     show Modal06BasicInformationWidget;
 import 'package:flutter/material.dart';
