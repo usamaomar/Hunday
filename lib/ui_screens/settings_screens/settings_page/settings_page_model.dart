9d0a041724beb47a924cf06b46fdedad0cb5235a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-30 10:13:34 +0000

    Updating to latest FlutterFlow output.

--- lib/ui_screens/settings_screens/settings_page/settings_page_model.dart
+++ lib/ui_screens/settings_screens/settings_page/settings_page_model.dart
@@ -9,7 +9,6 @@ import 'settings_page_widget.dart' show SettingsPageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:provider/provider.dart';
