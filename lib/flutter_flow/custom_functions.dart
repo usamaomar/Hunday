1d459f5dc75186ca0a339ca0f068df763ee556dc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-17 11:42:01 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/custom_functions.dart
+++ lib/flutter_flow/custom_functions.dart
@@ -398,7 +398,7 @@ CartItemModelStruct convertFromJsonToCartObject(dynamic jsonObject) {
       cartItems: convertFromJsonListToCarttList(jsonObject['cartItems']),
       subTotal: jsonObject['subTotal'].toDouble(),
       tax: jsonObject['tax'].toString(),
-      shippingCost: jsonObject['shippingCost'],
+      shippingCost: jsonObject['shippingCost'].toDouble(),
       discountRate: jsonObject['discountRate'].toString(),
       couponCode: jsonObject['couponCode'],
       totalPrice: jsonObject['totalPrice'].toDouble());
